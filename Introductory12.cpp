67895e0c6740a484c3318023a0a283de296ef742
Miyookii <joelaarontumala@gmail.com>
2026-01-11 14:05:00 +0800

    Update Introductory12.cpp

--- Introductory12.cpp
+++ Introductory12.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 int main() {
-    float pizzaRadius;
-    const float areaPerSlice = 14.125;
-    const float pi = 3.14;
+    int numberOfSlices;
+    float radiusOfPizza;
+    float areaOfPizza;
+    const float AREAOFSLICE = 14.13;
+    const float PI = 3.14;
 
     cout << "Enter the radius of the pizza: ";
-    cin >> pizzaRadius;
-    float pizzaArea = pi * pizzaRadius * pizzaRadius;
-    int numberOfSlices = static_cast<int>(pizzaArea / areaPerSlice);
+    cin >> radiusOfPizza;
+    areaOfPizza = PI * radiusOfPizza * radiusOfPizza;
+    numberOfSlices = static_cast<int>(areaOfPizza / AREAOFSLICE);
     cout << "The pizza can be cut into " << numberOfSlices << " slices." << endl;
     return 0;
+
 }
